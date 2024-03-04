19157054255e326bc80de7fddbef5487026c1cbe
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-03 22:56:41 -0300

    Module 02-aula 07

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,6 +3,7 @@ import Feed from "./assets/feed.svg";
 import Account from "./assets/account_circle.svg";
 import Info from "./assets/info.svg";
 import Logout from "./assets/logout.svg";
+import "./styles.css";
 
 export default function Sidebar() {
   return (
