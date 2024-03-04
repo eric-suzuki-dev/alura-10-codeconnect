db51dc109cf128ceffaab8f37cecd72be485837c
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-04 00:27:41 -0300

    Module 04-aula 04

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ import Sidebar from "./components/Sidebar";
 import Filtro from "./components/Filtro";
 import Ordenacao from "./components/Ordenacao";
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes')
+      .then(resposta => resposta.json())
+      .then(dados => setDados(dados))
+  }, [])
+
+  console.log(dados)
+
   return (
     <div className='container'>
       <Sidebar />
