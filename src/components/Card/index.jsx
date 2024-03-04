bcecb00441c45f470827bf5b47e655a06f9c76eb
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-04 00:49:25 -0300

    Module 04-aula 07

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,44 +1,51 @@
-import Capa from './assets/capa.png';
 import Code from './assets/code.svg';
 import Chat from './assets/chat.svg';
 import Share from './assets/share.svg';
-import Icone from './assets/icone.png';
 import './styles.css';
 
-export default function Card() {
-    return (
-        <article className="card">
-            <div className="card__imagem">
-                <img src={Capa} alt='imagem do post' />
-            </div>
-            <div className='card__conteudo'>
-    <div className='conteudo__texto'>
-        <h3>Título do post</h3>
-        <p>Resumo do post</p>
-    </div>
-</div>
+export default function Card({
+	id,
+	imagemUrl,
+	titulo,
+	resumo,
+	linhasDeCodigo,
+	compartilhamentos,
+	comentarios,
+	usuario,
+}) {
+	return (
+		<article className='card'>
+			<div className='card__imagem'>
+				<img src={imagemUrl} alt='imagem do post' />
+			</div>
+			<div className='card__conteudo'>
+				<div className='conteudo__texto'>
+					<h3>{titulo}</h3>
+					<p>{resumo}</p>
+				</div>
 
-<div className='conteudo__rodape'>
-    <ul>
-        <li>
-            <img src={Code} alt='códigos' />
-            100
-        </li>
-        <li>
-            <img src={Share} alt='compartilhamentos' />
-            12
-        </li>
-        <li>
-            <img src={Chat} alt='comentários' />
-            10
-        </li>
-    </ul>
-</div>
+				<div className='conteudo__rodape'>
+					<ul>
+						<li>
+							<img src={Code} alt='códigos' />
+							{linhasDeCodigo}
+						</li>
+						<li>
+							<img src={Share} alt='compartilhamentos' />
+							{compartilhamentos}
+						</li>
+						<li>
+							<img src={Chat} alt='comentários' />
+							{comentarios}
+						</li>
+					</ul>
 
-<div className='rodape__usuario'>
-    <img src={Icone} alt='imagem do usuário' />
-    @moni
-</div>
-        </article>
-    )
+					<div className='rodape__usuario'>
+						<img src={usuario.imagem} alt='imagem do usuário' />
+						{usuario.nome}
+					</div>
+				</div>
+			</div>
+		</article>
+	);
 }
